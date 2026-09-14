4c8eaa0b68a37738141f898fdfb0d63772be29c7
Mojtaba Charchian <mojtabacharchian@gmail.com>
2026-09-14 11:03:24 +0330

    add db layer

---
+++ backend_express/src/utils/db.ts
@@ -0,0 +1,26 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const dbPath = path.join(process.cwd(), "db.json");
+
+export interface ICourse {
+    id: string,
+    title: string,
+    description: string,
+    price: number,
+    student: number,
+    img: string
+};
+
+interface IDatabase {
+    courses: ICourse[]
+};
+
+export async function readDb(): Promise<IDatabase> {
+    const data = await fs.readFile(dbPath,'utf-8');
+    return JSON.parse(data);
+}
+
+export async function writeDb(data:IDatabase) {
+    await fs.writeFile(dbPath,JSON.stringify(data,null,2),'utf-8');
+}
